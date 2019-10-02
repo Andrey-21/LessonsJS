4e44c32519a51115c1331464f660c217d1810fa8
Andrey21 <andrey.morgun@proarea.co>
2019-10-02 14:07:03 -0700

    HW lesson 2 objects and array

---
+++ lesson 2/js/app.js
@@ -0,0 +1,13 @@
+//HW lesson 2 slide 8
+//1
+const objPhone = {
+    product: 'iPhone'
+};
+//2
+objPhone.price = 1000;
+objPhone.currency = 'dollar';
+//3
+objPhone.details = {
+    model: 'iPhone7',
+    color: 'Black'
+};
