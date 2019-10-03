b0ee46a68c0a221b54945bdc400f690779e87bc3
Andrey21 <andrey.morgun@proarea.co>
2019-10-03 16:46:10 -0700

    Add new HW for lesson 2

--- lesson 2/js/app.js
+++ lesson 2/js/app.js
@@ -11,3 +11,20 @@ objPhone.details = {
     model: 'iPhone7',
     color: 'Black'
 };
+
+// HW lesson 2 slide 10
+
+let string = 'i am in the easycode';
+let newStr = '';
+
+for (let i = 0; i < string.length; i++) {
+    if (i === 0) {
+        newStr += string[i].toUpperCase()
+    } else if (string[i] === ' ') {
+        newStr += string[i];
+        newStr += string[i + 1].toUpperCase();
+        i++
+    } else {
+        newStr += string[i]
+    }
+}
