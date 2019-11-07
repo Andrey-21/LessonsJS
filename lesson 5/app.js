11a5bb9a390225cbaa86c532c50d3276dde8c3a5
Andrey21 <andrey.morgun@proarea.co>
2019-11-07 16:45:41 +0200

    Add new HW for lesson 5

---
+++ lesson 5/app.js
@@ -0,0 +1,66 @@
+// HW lesson 5 Slide 7-9
+
+//1
+// const rectangle = {
+//   width: 300,
+//   height: 200,
+//   getSquare: function () {
+//     return 'площадь ' + this.width * this.height
+//
+//   }
+// };
+// rectangle.getSquare();
+
+//2
+
+// const price = {
+//   price: 10,
+//   discount: '15%',
+//   getPrice: function () {
+//
+//        return this.price
+//
+//   },
+//   getPriceWithDiscount: function () {
+//
+//     return this.price - (parseInt(this.discount) / 100 * this.price);
+//
+//   }
+// };
+//
+// console.log(price.getPrice());
+// console.log(price.getPriceWithDiscount());
+
+//3
+
+// const object = {
+//   height: 10,
+//   heightIcr: function () {
+//     return this.height + 1;
+//   }
+// };
+//
+// let newHeight = object.heightIcr();
+// console.log(newHeight);
+
+
+//4
+
+// const numerator = {
+//   value: 1,
+//   double: function () {
+//     return this.value * 2
+//   },
+//   plusOne: function () {
+//     return this.value + 1
+//   },
+//   minusOne: function () {
+//     return this.value - 1
+//   }
+// };
+//
+// console.log(numerator.double().plusOne().plusOne().minusOne());
+
+
+
+
